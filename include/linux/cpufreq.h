1aad51bc3ec6cfd1a82816d0330808b4e32d1cbd
Slawek <yarpiin@gmail.com>
2016-12-14 21:38:20 +0000

    add ironactive gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -526,6 +526,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
+extern struct cpufreq_governor cpufreq_gov_ironactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 #endif
 
 /*********************************************************************
